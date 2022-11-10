e9023ecd2d8daf6fbb21b4e9959d39466a08c668
Refanza Maulana <2031710167@student.polinema.ac.id>
2022-11-10 09:02:30 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+    $nama = 'Refan';
+    $umur = 20;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik = "Iyul";
+    $umurAdik = 15;
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berumur
+    $umurAdik tahun. <br>";
+
+    $selisihUmur = $umur - $umurAdik;
+
+    echo "Selisih usia saya dan adis saya adalah $selisihUmur tahun."
+
+
+
+?>
